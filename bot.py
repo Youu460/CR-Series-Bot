2550566a977cd27ad6e0b4377c4350517f9f185a
Leo MC <nisanox34@gmail.com>
2025-08-09 17:54:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,9 @@ from datetime import date, datetime
 import pytz
 import time
 
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
+
 PORT = "8080"
 
 class Bot(Client):
